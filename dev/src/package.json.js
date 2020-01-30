e3611d387b0ece354164da001dadf586ff6d8fea
Martin Winkler <martin@werk85.de>
2020-01-30 16:03:06 +0100

    Updated dependencies and npm scripts

--- dev/src/package.json.js
+++ dev/src/package.json.js
@@ -6,16 +6,16 @@ module.exports = (settings) => `
   "author": ${JSON.stringify(settings.author)},
   "private": true,
   "dependencies": {
+    "@fortawesome/fontawesome-free": "^5.10.2",
+    "bootstrap": "^4.3.1",
     "expose-loader": "^0.7.5",
     "jquery": "^3.3.1",
-    "bootstrap": "^4.3.1",
-    "popper.js": "^1.14.6",
-    "@fortawesome/fontawesome-free": "^5.10.2",
     "js-cookie": "^2.2.0",
-    "lightbox2": "^2.9.0"
+    "lightbox2": "^2.9.0",
+    "popper.js": "^1.14.6"
   },
   "devDependencies": {
-    "webpack": "^4.26.1"
+    "webpack": "^4.41.5"
   }
 }
 `;
